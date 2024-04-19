91638d0df95027375069cb9b83610997789b03e2
vakkaskarakurt <karakurtvakkas@gmail.com>
2024-04-20 01:45:36 +0300

    .

--- new.py
+++ new.py
@@ -21,3 +21,18 @@ ignoreLetters = ['?','!','.',',']
 for intent in intents['intents']:
     for pattern in intent ['patterns']:
         wordList = nltk.word_tokenize
+        words.extend(wordList)
+        documents.append((wordList, intent['tag']))
+        
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(word) 
+         for word in words 
+         if word not in ignoreLetters]
+
+words = sorted(set(classes))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
